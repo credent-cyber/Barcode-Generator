48ac8906375edf033d7739955538f81bb1db63a3
chhagan sinha <chhagan.sinha@credentinfotech.com>
2024-08-06 16:55:10 +0530

    with text or number below of barcode

--- Barcode Generator/Program.cs
+++ Barcode Generator/Program.cs
@@ -21,19 +21,34 @@ class Program
 
         var pixelData = writer.Write("1234567890");
 
-        using (var bitmap = new Bitmap(pixelData.Width, pixelData.Height, PixelFormat.Format32bppArgb))
+        using (var bitmap = new Bitmap(pixelData.Width, pixelData.Height + 30)) // Extra space for text
+        using (var graphics = Graphics.FromImage(bitmap))
         {
-            var bitmapData = bitmap.LockBits(new Rectangle(0, 0, pixelData.Width, pixelData.Height), ImageLockMode.WriteOnly, PixelFormat.Format32bppArgb);
+            // Draw the barcode
+            var barcodeBitmap = new Bitmap(pixelData.Width, pixelData.Height, PixelFormat.Format32bppArgb);
+            var barcodeData = barcodeBitmap.LockBits(new Rectangle(0, 0, pixelData.Width, pixelData.Height), ImageLockMode.WriteOnly, PixelFormat.Format32bppArgb);
             try
             {
-                System.Runtime.InteropServices.Marshal.Copy(pixelData.Pixels, 0, bitmapData.Scan0, pixelData.Pixels.Length);
+                System.Runtime.InteropServices.Marshal.Copy(pixelData.Pixels, 0, barcodeData.Scan0, pixelData.Pixels.Length);
             }
             finally
             {
-                bitmap.UnlockBits(bitmapData);
+                barcodeBitmap.UnlockBits(barcodeData);
             }
 
-            bitmap.Save("barcode.png", ImageFormat.Png);
+            graphics.DrawImage(barcodeBitmap, 0, 0);
+
+            // Draw the text below the barcode
+            var font = new Font("Arial", 12);
+            var brush = Brushes.Black;
+            var text = "1234567890";
+            var textSize = graphics.MeasureString(text, font);
+            var textX = (bitmap.Width - textSize.Width) / 2; // Center the text
+            var textY = bitmap.Height - 20; // Position text 20 pixels below the barcode
+            graphics.DrawString(text, font, brush, textX, textY);
+
+            // Save the image
+            bitmap.Save("barcode_with_text.png", ImageFormat.Png);
         }
     }
 }
