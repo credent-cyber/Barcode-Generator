307b5c91acf37cb3077dd79c641dd6cb78c45f03
chhagan sinha <chhagan.sinha@credentinfotech.com>
2024-08-06 16:50:43 +0530

    Add project files.

---
+++ Barcode Generator/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using ZXing;
+using ZXing.Common;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var writer = new BarcodeWriterPixelData
+        {
+            Format = BarcodeFormat.CODE_128,
+            Options = new EncodingOptions
+            {
+                Height = 100,
+                Width = 300,
+                Margin = 10
+            }
+        };
+
+        var pixelData = writer.Write("1234567890");
+
+        using (var bitmap = new Bitmap(pixelData.Width, pixelData.Height, PixelFormat.Format32bppArgb))
+        {
+            var bitmapData = bitmap.LockBits(new Rectangle(0, 0, pixelData.Width, pixelData.Height), ImageLockMode.WriteOnly, PixelFormat.Format32bppArgb);
+            try
+            {
+                System.Runtime.InteropServices.Marshal.Copy(pixelData.Pixels, 0, bitmapData.Scan0, pixelData.Pixels.Length);
+            }
+            finally
+            {
+                bitmap.UnlockBits(bitmapData);
+            }
+
+            bitmap.Save("barcode.png", ImageFormat.Png);
+        }
+    }
+}
